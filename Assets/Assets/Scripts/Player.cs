aac45f35d905f4124d65a88f8d13fad7c160d9bf
Carlos Ruminott <carlosruminott@gmail.com>
2022-02-28 13:24:53 -0300

    script de impulso con input "Jump"

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 100f;
+    public float impulse = 10f;
 
     public Rigidbody2D rb;
 
+    private bool moveReady = true;
+    private bool impulseReady = true;
+
+
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0) * speed * Time.fixedDeltaTime;
+        if(impulseReady && Input.GetButtonDown("Jump"))
+        {
+            moveReady = false;
+            impulseReady = false;
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0) * speed * impulse * Time.fixedDeltaTime;
+            StartCoroutine(StopImpulse());
+            StartCoroutine(ReadyToImpusle());
+        }
+
+        if(moveReady)
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0) * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    IEnumerator StopImpulse()
+    {
+        yield return new WaitForSeconds(0.2f);
+        moveReady = true;
+        rb.velocity = Vector2.zero;
+        Debug.Log("stop impulse");
+    }
+
+    IEnumerator ReadyToImpusle()
+    {
+        yield return new WaitForSeconds(5);
+        impulseReady = true;
+        Debug.Log("ready to impulse");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
